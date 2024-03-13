26552125c2881f5957353fbd321a8708e88786e8
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-13 16:32:07 +0600

    Several Improvements!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Book from './Book';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    fetch('product.json')
+      .then(res => res.json())
+      .then(data => setProduct(data))
+  }, []);
   return (
     <>
-      
+      <div className='container mx-auto mt-24 flex gap-6'>
+        <div className='grid grid-cols-3 gap-8'>
+          {
+            product.map(books => (
+              <Book key={books.id} book={books}></Book>
+            ))
+          }
+        </div>
+        <div className='basis-1/3'>
+          <div className='border rounded-xl'>
+            <div className="overflow-x-auto">
+              <table className="table text-center">
+                <thead>
+                  <tr className='text-base'>
+                    <th>Name</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/Book.jsx
@@ -0,0 +1,25 @@
+function Book({ book }) {
+    const { name, image, originalPrice, discountPrice } = book;
+    
+    const handleCart = cart =>{
+        console.log(cart)
+    }
+    return (
+        <div>
+            <div className="card card-compact bg-base-100 p-6 border-2 border-dashed border-amber-200">
+                <figure><img className="rounded-2xl h-96 w-96 object-cover" src={image} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title text-lg">{name}</h2>
+                    <div className="flex gap-4 items-center text-lg font-bold">
+                        <span className="line-through">{originalPrice} Tk</span><span className="rounded-xl bg-yellow-200 px-2 py-2">{discountPrice} Tk</span>
+                    </div>
+                    <div className="card-actions justify-start">
+                        <button onClick={()=>{handleCart(book)}} className="btn bg-gradient-to-r from-green-400 to-blue-500 px-8 text-white text-base hover:from-pink-500 hover:to-yellow-500">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Book;
